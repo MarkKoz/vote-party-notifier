4fd05f6bb0bbb3601c27eb00a510d1543fecc0c2
MarkKoz <KozlovMark@gmail.com>
2019-01-20 22:34:00 -0600

    Parse the votes from the HTML response

--- votepartynotifier/notifier.py
+++ votepartynotifier/notifier.py
@@ -2,6 +2,7 @@ import signal
 import sys
 import time
 
+from bs4 import BeautifulSoup
 import requests
 
 POLL_RATE = 5
@@ -12,6 +13,19 @@ def sigint_handler(signalnum, frame):
     sys.exit(0)
 
 
+def parse(html) -> int:
+    soup = BeautifulSoup(html, "html.parser")
+    result = soup.find("h3")
+
+    if not result:
+        raise RuntimeError("Failed to parse the HTML response.")
+
+    try:
+        votes = result.string.replace(",", "")
+        return int(votes)
+    except ValueError:
+        raise ValueError("Could not convert the votes to an integer.")
+
 def main():
     signal.signal(signal.SIGINT, sigint_handler)
 
@@ -20,7 +34,8 @@ def main():
             response = session.get(ENDPOINT)
             response.raise_for_status()
 
-            print(response.text)
+            votes = parse(response.text)
+            print(votes)
             sys.stdout.flush()
 
             time.sleep(POLL_RATE)
