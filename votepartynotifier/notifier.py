f73406841950b4ed04ad7c3e22c74be5e6fb50d3
MarkKoz <KozlovMark@gmail.com>
2019-01-20 22:07:06 -0600

    Poll the API

--- votepartynotifier/notifier.py
+++ votepartynotifier/notifier.py
@@ -1,5 +1,11 @@
 import signal
 import sys
+import time
+
+import requests
+
+POLL_RATE = 5
+ENDPOINT = "http://blissscape.net/vote/templates/stats_index.php"
 
 
 def sigint_handler(signalnum, frame):
@@ -9,3 +15,12 @@ def sigint_handler(signalnum, frame):
 def main():
     signal.signal(signal.SIGINT, sigint_handler)
 
+    with requests.Session() as session:
+        while True:
+            response = session.get(ENDPOINT)
+            response.raise_for_status()
+
+            print(response.text)
+            sys.stdout.flush()
+
+            time.sleep(POLL_RATE)
